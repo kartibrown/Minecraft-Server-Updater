832a014b4fcce71976c5bd652f43229614d382c9
Adam <69228473+kartibrown@users.noreply.github.com>
2021-11-21 01:53:36 +0100

    Update MinecraftServerUpdater.java

--- src/MinecraftServerUpdater.java
+++ src/MinecraftServerUpdater.java
@@ -1,10 +1,10 @@
 import graphics.MyWindow;
-import graphics.menu.MainMenu;
 
 public class MinecraftServerUpdater
 {
 	public final static void main(final String[] args)
 	{
 		new MyWindow();
+
 	}
 }
