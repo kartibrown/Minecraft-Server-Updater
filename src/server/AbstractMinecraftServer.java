cb433937e935dbe272addf112837dba1926514d3
Adam <69228473+kartibrown@users.noreply.github.com>
2021-11-22 03:41:07 +0100

    Update AbstractMinecraftServer.java

--- src/server/AbstractMinecraftServer.java
+++ src/server/AbstractMinecraftServer.java
@@ -2,18 +2,35 @@ package server;
 
 abstract class AbstractMinecraftServer implements MinecraftServer
 {
-	protected String dir;
+	protected String dir, name, encPassword, version;
+	protected int ram;
 
 	protected AbstractMinecraftServer()
 	{ dir = ""; }
 
 	protected AbstractMinecraftServer(final String toString)
-	{
-		dir = "";
-	}
+	{ dir = ""; }
 
 	@Override
 	public final String toString()
-	{ return this.getClass().getSimpleName() + ": "; }
+	{
+		return this.getClass().getSimpleName() + ": " + name + ", "
+				+ encPassword + ", " + ram;
+	}
 
+	/*
+	 * GETTERS & SETTERS
+	 */
+
+	@Override
+	public final String getVersion()
+	{ return this.version; }
+
+	@Override
+	public final void setVersion(final String version)
+	{ this.version = version; }
+	
+	@Override
+	public final String getServerDir()
+	{ return dir; }
 }
