cfe167d066ce2f1b2abdd4778b20b080d5cab206
Adam <69228473+kartibrown@users.noreply.github.com>
2021-11-25 03:01:37 +0100

    Update MinecraftServer.java

--- src/server/MinecraftServer.java
+++ src/server/MinecraftServer.java
@@ -2,10 +2,18 @@ package server;
 
 public interface MinecraftServer
 {
+	
+	/*
+	 * GETTERS & SETTERS
+	 */
+
 	public String getServerType();
-	
+
 	public String getServerDir();
-	
+
+	public void setServerDir(final String dir);
+
 	public String getVersion();
+
 	public void setVersion(final String version);
 }
